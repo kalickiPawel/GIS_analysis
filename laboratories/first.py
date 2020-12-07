9ac7e8043fbae399f02e585fd8f5a252a4e6a765
Paweł <kalicki.paul@icloud.com>
2020-12-07 11:32:27 +0100

    :poop: Added moving average

--- laboratories/first.py
+++ laboratories/first.py
@@ -6,28 +6,45 @@ from sklearn.neighbors import KDTree
 
 
 class Interpolation:
-    def __init__(self, spacing=0.1, data_path="./data", filename='wraki utm.txt', window_type=0, window_size=0.1, num_min_points=2):
-        self.window_type = window_type  # 0 -> square / 1 -> circle
-        self.window = window_size if window_type else (window_size, window_size)
-        self.spacing = spacing  # 0.1 -> wraki / 1 -> pozostałe
-        self.num_min_points = num_min_points
+    def __init__(self, data_path="./data", filename='wraki utm.txt', **kwargs):
+        self.data = self.load_data(data_path, filename)
 
+        self.spacing = None
+        self.is_square = None
+        self.is_circle = None
+        self.window_size = None
+        self.num_min_points = None
+
+        valid_keys = ["spacing", "is_square", "is_circle", "window_size", "num_min_points"]
+        for key in valid_keys:
+            setattr(self, key, kwargs.get(key))
+
+        self.grid = self.get_grid()
+        self.zz = self.interp_moving_average()
+        xx, yy = self.grid
+        self.plot_surf(xx, yy, self.zz)
+        self.plot()
+
+    def load_data(self, data_path, filename):
         df = pd.read_csv(os.path.join(data_path, filename), sep=" ", header=None)
         df = df.dropna(axis=1)
         df.columns = ["Lat", "Long", "depth"]
-        self.data = df
-        self.grid = self.get_grid()
-        self.grid_positions = self.get_grid(pos=True)
-        self.grid_size = len(self.grid)
-        self.z = self.interp_moving_average()
+        return df
 
     def interp_moving_average(self):
-        tree = KDTree(self.data[['Lat', 'Long']].to_numpy())
-        radius_length = 0.4
-        for i in range(self.grid.shape[2]):
-            for j in range(self.grid.shape[1]):
-                ids = tree.query_radius([[452606.36, 5967819.4]], r=radius_length)
-                zz = 0
+        xy_data = self.data[['Lat', 'Long']].to_numpy()
+        data = self.data[['Lat', 'Long', 'depth']].to_numpy()
+        tree = KDTree(xy_data)
+        radius_length = self.window_size
+        xx, yy = self.grid
+        zz = np.empty((xx.shape[0], xx.shape[1]))
+        for i in range(xx.shape[0]):
+            for j in range(xx.shape[1]):
+                ids = tree.query_radius([[xx[i, j], yy[i, j]]], r=radius_length)
+                if len(ids[0]) == 0:
+                    zz[i, j] = np.nan
+                    continue
+                zz[i, j] = np.mean(data[ids[0], 2])
         return zz
 
     def interp_idw(self):
@@ -42,9 +59,14 @@ class Interpolation:
         x = np.arange(mins['Lat'], maxes['Lat'], self.spacing)
         y = np.arange(mins['Long'], maxes['Long'], self.spacing)
         mesh = np.meshgrid(x, y)
-        return np.array(mesh) if not pos else np.vstack(list(map(np.ravel, mesh)))
+        return mesh if not pos else np.vstack(list(map(np.ravel, mesh)))
 
-    def plot(self, dim=0):
+    def plot_surf(self, xx, yy, zz):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(xx, yy, zz)
+
+    def plot(self):
         # 0 - 2d
 
         # plt.figure()
@@ -52,7 +74,7 @@ class Interpolation:
         # plt.show()
 
         plt.figure()
-        p = plt.imshow(self.z)
+        p = plt.imshow(self.zz)
         plt.colorbar(p)
         plt.show()
 
--- main.py
+++ main.py
@@ -2,5 +2,13 @@ from laboratories import Interpolation
 
 
 if __name__ == '__main__':
-    first = Interpolation()
-    # print(first.data)
+    first = Interpolation(
+        data_path="./data",
+        filename='wraki utm.txt',
+        spacing=0.1,
+        window_type=0,
+        window_size=0.1,
+        num_min_points=2
+    )
+    # print(vars(first))
+
