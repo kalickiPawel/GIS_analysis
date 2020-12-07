bc311e74620f680739817e364cdc5fa4cec0ead0
Paweł <kalicki.paul@icloud.com>
2020-12-07 11:50:46 +0100

    :bug: Changed parameter for use less nan values

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ if __name__ == '__main__':
         filename='wraki utm.txt',
         spacing=0.1,
         window_type=0,
-        window_size=0.1,
+        window_size=0.2,
         num_min_points=2
     )
     # print(vars(first))
-
