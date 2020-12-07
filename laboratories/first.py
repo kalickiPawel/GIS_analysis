7a8d0c22ecbd7a1d98872d415715db2bfcc339c5
Paweł <kalicki.paul@icloud.com>
2020-12-07 11:50:25 +0100

    :bug: repaired interpolation

--- laboratories/first.py
+++ laboratories/first.py
@@ -7,8 +7,7 @@ from sklearn.neighbors import KDTree
 
 class Interpolation:
     def __init__(self, data_path="./data", filename='wraki utm.txt', **kwargs):
-        self.df = self.load_data(data_path, filename)
-        self.data = self.df.to_numpy()
+        self.data = self.load_data(data_path, filename)
 
         self.spacing = None
         self.is_square = None
@@ -22,9 +21,9 @@ class Interpolation:
 
         self.grid = self.get_grid()
         self.zz = self.interp_moving_average()
-
+        xx, yy = self.grid
+        self.plot_surf(xx, yy, self.zz)
         self.plot()
-        self.plot(surf=True)
 
     def load_data(self, data_path, filename):
         df = pd.read_csv(os.path.join(data_path, filename), sep=" ", header=None)
@@ -33,16 +32,19 @@ class Interpolation:
         return df
 
     def interp_moving_average(self):
-        tree = KDTree(self.data[:, :2])
+        xy_data = self.data[['Lat', 'Long']].to_numpy()
+        data = self.data[['Lat', 'Long', 'depth']].to_numpy()
+        tree = KDTree(xy_data)
+        radius_length = self.window_size
         xx, yy = self.grid
         zz = np.empty((xx.shape[0], xx.shape[1]))
         for i in range(xx.shape[0]):
             for j in range(xx.shape[1]):
-                ids = tree.query_radius([[xx[i, j], yy[i, j]]], r=self.window_size)
+                ids = tree.query_radius([[xx[i, j], yy[i, j]]], r=radius_length)
                 if len(ids[0]) == 0:
                     zz[i, j] = np.nan
                     continue
-                zz[i, j] = np.mean(self.data[ids[0], 2])
+                zz[i, j] = np.mean(data[ids[0], 2])
         return zz
 
     def interp_idw(self):
@@ -52,21 +54,34 @@ class Interpolation:
         pass
 
     def get_grid(self, pos=False):
-        mins, maxes = self.df.min(), self.df.max()
+        mins = self.data.min()
+        maxes = self.data.max()
         x = np.arange(mins['Lat'], maxes['Lat'], self.spacing)
         y = np.arange(mins['Long'], maxes['Long'], self.spacing)
         mesh = np.meshgrid(x, y)
         return mesh if not pos else np.vstack(list(map(np.ravel, mesh)))
 
-    def plot(self, surf=False):
+    def plot_surf(self, xx, yy, zz):
         fig = plt.figure()
-        if surf:
-            ax = fig.add_subplot(111, projection='3d')
-            ax.plot_surface(self.grid[0], self.grid[1], self.zz)
-        else:
-            p = plt.imshow(self.zz)
-            plt.colorbar(p)
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(xx, yy, zz)
+
+    def plot(self):
+        # 0 - 2d
+
+        # plt.figure()
+        # plt.plot(df['Lat'], df['Long'])
+        # plt.show()
+
+        plt.figure()
+        p = plt.imshow(self.zz)
+        plt.colorbar(p)
         plt.show()
+
+        # fig = plt.figure()
+        # ax = fig.add_subplot(111, projection='3d')
+        # ax.plot_surface(self.grid[0], self.grid[1], zz)
+        # plt.show()
 
 # TODO:
     # 7. -> progress bar
